4c1ab35c4ee054080bf565e1ed46161a4fc510c4
Hristijan <kde.chris@gmail.com>
2020-12-28 10:37:47 +0100

    add bootstrap partials

--- wp-content/themes/twentytwenty/less/gulpfile.js
+++ wp-content/themes/twentytwenty/less/gulpfile.js
@@ -2,29 +2,26 @@ var gulp = require('gulp');
 var gulpless = require('gulp-less');
 var gulpautoprefixer = require('gulp-autoprefixer');
 var watch = require('gulp-watch');
-//
-//   gulp.task('styles',function(){
-//           var srcfile = './src/styles.less';
-//              return gulp
-//                      .src(srcfile)
-//                      .pipe(gulpless())
-//                      .pipe(gulpautoprefixer({browsers: ['last 2 versions','>5%']}))
-//                      .pipe(gulp.dest("./css/"));
-//   });
-//
-// gulp.task('watch', function() {
-//     gulp.watch('src/*.less', ['less'])
-// });
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
 
 gulp.task('less', function() {
     return gulp
-        .src('./src/*.less')
+        .src('./src/bootstrap.less', './src/bootstrap-grid.less', './src/bootstrap-reboot.less')
         .pipe(gulpless())
-        .pipe(gulpautoprefixer({browsers: ['last 2 versions','>5%']}))
+        .pipe(gulpautoprefixer({browsers: ['last 2 versions','>5%']})
+        .pipe(concat("styles.css")))
         .pipe(gulp.dest("./css/"));
+});
+
+gulp.task('pack-css', function () {
+    return gulp.src(['./css/bootstrap.css', './css/bootstrap-grid.css', './css/bootstrap-reboot.css'])
+        .pipe(concat('styles.css'))
+        .pipe(rename('theme.css'))
+        .pipe(gulp.dest('../'));
 });
 
 gulp.task('watch', function() {
     return gulp
-        .watch("./src/*.less", gulp.series("less"))
+        .watch("./src/*.less", gulp.series("less", "pack-css"))
 });
